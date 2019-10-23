1ce1a3c6a1f545d1ffafc0ec527087c82f15aefa
tuyengabi <tuyen.gabi@gmail.com>
2019-10-23 15:59:35 +0200

    delete profile-navbar, add file navbar and footer

---
+++ nav et footer/asset/js/js.js
@@ -0,0 +1,38 @@
+
+scrollToView();
+
+function scrollToView(){
+    setTimeout(function() {
+        document.getElementById('myDiv').scrollIntoView({behavior: "smooth"});      
+        pause();    
+    }, 3000);    
+}
+
+function pause(){
+    setTimeout(function() {         
+        var elmnt = document.getElementById("video");
+        elmnt.scrollIntoView(true);  
+        document.getElementById("video").remove();     
+    }, 1000);    
+}
+
+// fade section
+
+$(function(){
+	$(window).scroll( function(){
+		
+		var bottom_of_window = $(window).scrollTop() + $(window).height();
+		//fade-in
+		$('.fade-ani').each(function(){
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			 if( bottom_of_window > bottom_of_object ){
+				$(this).addClass('showing');
+			}
+			else{
+				//$(this).removeClass('showing');
+			}
+		});
+
+	});
+});
+
